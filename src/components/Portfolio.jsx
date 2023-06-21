f52b01cd609116ae797602f654bc89b5cb64f520
Turalyusubov <tu201906152@code.edu.az>
2023-06-21 19:32:04 +0400

    added show more btn in portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Project from './Project'
 import { projects } from '../projects'
 
 export default function Portfolio() {
+    const [itemCount, setitemCount] = useState(3)
+    const itemsToShow = projects.slice(itemCount).reverse()
+
+    const showMore = () => {
+        if (itemCount === 3) {
+            setitemCount(0)
+        } else {
+            setitemCount(3)
+        }
+    }
+
     return (
         <div className='bg-main-green text-stone-100 relative flex flex-col justify-center items-center py-20 h-full w-full px-6 md:px-32'>
-            <h2 className='font-bold mb-20 text-4xl'>My <span className='text-light-green drop-shadow-light'>Portfolio</span></h2>
+            <h2 className='font-bold mb-8 text-4xl group'>My <span className='text-light-green drop-shadow-light group-hover:text-bright-green group-hover:drop-shadow-bright duration-300'>Portfolio</span></h2>
             <div className="w-full projects grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                {projects.map(project =>
+                {itemsToShow.map(project =>
                     <Project
                         key={project.id}
                         title={project.title}
@@ -18,6 +29,8 @@ export default function Portfolio() {
                     />
                 )}
             </div>
+            {itemCount === 3 && <button className='mt-8 border-2 rounded-md w-full md:w-max bg-transparent text-light-green drop-shadow-light font-bold md:px-8 py-2 border-light-green hover:border-bright-green hover:text-bright-green hover:drop-shadow-bright duration-300' onClick={() => setitemCount(0)}>Show More</button>}
+            {itemCount !== 3 && <p className='mt-8 italic text-stone-300'>More in my <a className='text-light-green drop-shadow-light hover:text-bright-green hover:drop-shadow-bright duration-300' target='_blank' href="https://github.com/Turalyusubov">Github</a></p>}
         </div>
     )
 }
--- src/projects.js
+++ src/projects.js
@@ -7,30 +7,6 @@ import TypingCover from './assets/typing-cover.png'
 
 export const projects = [
     {
-        id: 1,
-        title: 'ACME',
-        about: "You can't go on an adventure without ACME products",
-        github: 'https://github.com/Turalyusubov/acme-project',
-        live: 'https://acme-project-by-tural.netlify.app/',
-        cover: AcmeCover
-    },
-    {
-        id: 2,
-        title: 'Judico',
-        about: 'Looking for  a lawyer? Judico is here!',
-        github: 'https://github.com/Turalyusubov/judico-website',
-        live: 'https://judico-by-tural.netlify.app/',
-        cover: JudicoCover
-    },
-    {
-        id: 3,
-        title: 'Fitzone',
-        about: 'Great app for working out',
-        github: 'https://github.com/Turalyusubov/fitzone-app',
-        live: 'https://fitzone-by-tural.netlify.app/',
-        cover: FitzoneCover
-    },
-    {
         id: 4,
         title: 'RUSENERGOSTROY',
         about: 'Website for a building company',
@@ -53,5 +29,29 @@ export const projects = [
         github: 'https://github.com/Turalyusubov/speed-typing-game',
         live: 'https://speed-typing-by-tural.netlify.app/',
         cover: TypingCover
+    },
+    {
+        id: 1,
+        title: 'ACME',
+        about: "You can't go on an adventure without ACME products",
+        github: 'https://github.com/Turalyusubov/acme-project',
+        live: 'https://acme-project-by-tural.netlify.app/',
+        cover: AcmeCover
+    },
+    {
+        id: 2,
+        title: 'Judico',
+        about: 'Looking for  a lawyer? Judico is here!',
+        github: 'https://github.com/Turalyusubov/judico-website',
+        live: 'https://judico-by-tural.netlify.app/',
+        cover: JudicoCover
+    },
+    {
+        id: 3,
+        title: 'Fitzone',
+        about: 'Great app for working out',
+        github: 'https://github.com/Turalyusubov/fitzone-app',
+        live: 'https://fitzone-by-tural.netlify.app/',
+        cover: FitzoneCover
     },
 ]
